72eb1b8d3c129b2fb23674db651ac4e7eab1d96d
Dan Vega <danvega@gmail.com>
2022-03-08 21:21:08 -0500

    api endpoint now returns all config values

--- src/main/java/dev/danvega/secrets/NotionController.java
+++ src/main/java/dev/danvega/secrets/NotionController.java
@@ -4,21 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/notion")
 public class NotionController {
 
-    private final NotionConfigProperties notionConfigProperties;
+    private final NotionConfigProperties notionConfig;
 
     public NotionController(NotionConfigProperties notionConfigProperties) {
-        this.notionConfigProperties = notionConfigProperties;
+        this.notionConfig = notionConfigProperties;
     }
 
     @GetMapping
-    public void printAllProps() {
-        System.out.println(notionConfigProperties.apiUrl());
-        System.out.println(notionConfigProperties.apiVersion());
-        System.out.println(notionConfigProperties.authToken());
-        System.out.println(notionConfigProperties.databaseId());
+    public Map<String,String> printAllProps() {
+        return Map.of("apiUrl", notionConfig.apiUrl(),
+                "apiVersion", notionConfig.apiUrl(),
+                "authToken", notionConfig.authToken(),
+                "databaseId", notionConfig.databaseId());
     }
 }
